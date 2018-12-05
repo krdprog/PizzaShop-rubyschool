f0645d77b45115729602ec45ee97e4fe340f4d51
krdprog <info@krdprog.ru>
2018-12-05 08:51:40 +0300

    Add migrate and table products

--- app/app.rb
+++ app/app.rb
@@ -4,9 +4,8 @@ require 'sinatra/activerecord'
 
 set :database, "sqlite3:database.db"
 
-# class Client < ActiveRecord::Base
-# 	validates :name, presence: true, length: { minimum: 3 }
-# end
+class Product < ActiveRecord::Base
+end
 
 get '/' do
 	erb :index
