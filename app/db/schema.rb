ad180139589ad3f1a05c1af6b5fc90faee2a27eb
krdprog <info@krdprog.ru>
2018-12-05 09:51:37 +0300

    Create new migration with product items

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_045911) do
+ActiveRecord::Schema.define(version: 2018_12_05_060958) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
